9aa9ae2d410657232f167aad98a2b0aebeaea9fe
Mark Robertson <markrobertson67@gmail.com>
2024-06-09 16:14:33 -0400

    Added routes to time card current, index, create.

---
+++ src/components/pages/currentTimeCard/currentTimeCard.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function currentTimeCard() {
+  return (
+    <div>
+      <h2>Current Time Card</h2>
+      {/* Add your content for the current time card */}
+    </div>
+  );
+}
+
+export default currentTimeCard;
+
