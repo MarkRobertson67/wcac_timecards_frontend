4858195468577e35fb5f11f9a8ccc053eda97a1c
Mark Robertson <markrobertson67@gmail.com>
2024-06-09 17:12:45 -0400

    refixing file names

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from './components/footer/Footer';
 
 import CurrentTimeCard from './components/pages/currentTimeCard/CurrentTimeCard';
 import CreateNewTimeCard from './components/pages/createNewTimecard/CreatenewTimecard'; 
+import CreateNewTimeCard from './components/pages/createNewTimecard/CreatenewTimecard';
 import TimeCardIndex from './components/pages/timeCardsIndex/TimeCardsIndex'; 
 
 function App() {
