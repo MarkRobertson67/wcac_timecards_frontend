bcb89b9ae72ad96bcc6b7edf95eb93ad3277aa85
Mark Robertson <markrobertson67@gmail.com>
2024-07-15 00:31:46 -0400

    Firebase will be in the backend

---
+++ src/components/auth/FireBase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFb8ThW3YeaPloT3bBEvNP9bkaEFEnTRo",
+  authDomain: "wcac-authentication.firebaseapp.com",
+  projectId: "wcac-authentication",
+  storageBucket: "wcac-authentication.appspot.com",
+  messagingSenderId: "876702621962",
+  appId: "1:876702621962:web:f74002e3f8c65f3bd71af3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
