c39dd94eac4530d9f1bb6bfec8d1b71ef7aafb02
Mark Robertson <markrobertson67@gmail.com>
2024-11-25 19:14:00 -0500

    Updated Employee component for employee and isAdmin

--- src/components/pages/Employees/EmployeeDetails.js
+++ src/components/pages/Employees/EmployeeDetails.js
@@ -2,10 +2,18 @@
    Copyright (c) 2024 Mark Robertson
    See LICENSE.txt file for details. */
 
-
-import React, { useEffect, useState, useCallback } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Container, Row, Col, Form, Button, Spinner, Card } from 'react-bootstrap';
+import React, { useEffect, useState, useCallback } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Spinner,
+  Card,
+} from "react-bootstrap";
+import { auth } from "../../../firebase/firebaseConfig";
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -13,6 +21,7 @@ function EmployeeDetails() {
   const { id } = useParams(); // Get employee ID from URL
   const navigate = useNavigate();
   const [employee, setEmployee] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   // Fetch employee details based on ID
@@ -20,10 +29,19 @@ function EmployeeDetails() {
     setIsLoading(true);
 
     try {
-      const response = await fetch(`${API}/employees/${id}`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch employee details");
+      const user = auth.currentUser; // Get current user
+      if (!user) throw new Error("User not authenticated.");
+
+      // Fetch the current user's profile to determine admin status
+      const userResponse = await fetch(`${API}/employees/firebase/${user.uid}`);
+      const userData = await userResponse.json();
+      if (userData?.data) {
+        setIsAdmin(userData.data.is_admin);
       }
+
+      // Fetch employee details
+      const response = await fetch(`${API}/employees/${id}`);
+      if (!response.ok) throw new Error("Failed to fetch employee details");
 
       const data = await response.json();
       if (data && data.data) {
@@ -47,47 +65,60 @@ function EmployeeDetails() {
     const { name, value } = e.target;
     setEmployee((prevEmployee) => ({
       ...prevEmployee,
-      [name]: name === 'is_admin' ? value === 'true' : value,
+      [name]: name === "is_admin" ? value === "true" : value,
     }));
   };
 
   const handleSave = async () => {
-    if (window.confirm('Are you sure you want to save these changes?')) {
+    if (window.confirm("Are you sure you want to save these changes?")) {
       try {
+        // Send only editable fields for employees
+        const dataToUpdate = isAdmin
+          ? employee
+          : {
+              first_name: employee.first_name,
+              last_name: employee.last_name,
+              phone: employee.phone,
+            };
+
         const response = await fetch(`${API}/employees/${id}`, {
-          method: 'PUT',
+          method: "PUT",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(employee),
+          body: JSON.stringify(dataToUpdate),
         });
 
         if (!response.ok) {
-          throw new Error('Failed to update employee details');
+          throw new Error("Failed to update employee details");
         }
-        alert('Employee details updated successfully!');
+        alert("Employee details updated successfully!");
       } catch (error) {
-        console.error('Error updating employee details:', error);
-        alert('Failed to update employee details.');
+        console.error("Error updating employee details:", error);
+        alert("Failed to update employee details.");
       }
     }
   };
 
   const handleDelete = async () => {
-    if (window.confirm('Are you sure you want to delete this employee? This action cannot be undone.')) {
+    if (
+      window.confirm(
+        "Are you sure you want to delete this employee? This action cannot be undone."
+      )
+    ) {
       try {
         const response = await fetch(`${API}/employees/${id}`, {
-          method: 'DELETE',
+          method: "DELETE",
         });
 
         if (!response.ok) {
-          throw new Error('Failed to delete employee');
+          throw new Error("Failed to delete employee");
         }
-        alert('Employee deleted successfully!');
-        navigate('/employees');
+        alert("Employee deleted successfully!");
+        navigate("/employees");
       } catch (error) {
-        console.error('Error deleting employee:', error);
-        alert('Failed to delete employee.');
+        console.error("Error deleting employee:", error);
+        alert("Failed to delete employee.");
       }
     }
   };
@@ -107,10 +138,20 @@ function EmployeeDetails() {
   }
 
   return (
-    <Container className="mt-4 d-flex justify-content-center" style={{ paddingBottom: '100px' }}>
-      <Card style={{ width: window.innerWidth < 600 ? '100%' : '800px', padding: '15px' }}>
+    <Container
+      className="mt-4 d-flex justify-content-center"
+      style={{ paddingBottom: "100px" }}
+    >
+      <Card
+        style={{
+          width: window.innerWidth < 600 ? "100%" : "800px",
+          padding: "15px",
+        }}
+      >
         <Card.Body>
-          <h4 className="text-center mb-4">Employee Details for {employee.first_name} {employee.last_name}</h4>
+          <h4 className="text-center mb-4">
+            Employee Details for {employee.first_name} {employee.last_name}
+          </h4>
           <Form>
             <Row className="mb-2">
               <Col xs={12} md={6}>
@@ -122,6 +163,7 @@ function EmployeeDetails() {
                     value={employee.first_name}
                     onChange={handleChange}
                     size="sm"
+                    disabled={!isAdmin} // Non-admins can't edit this
                   />
                 </Form.Group>
               </Col>
@@ -134,6 +176,7 @@ function EmployeeDetails() {
                     value={employee.last_name}
                     onChange={handleChange}
                     size="sm"
+                    disabled={!isAdmin} // Non-admins can't edit this
                   />
                 </Form.Group>
               </Col>
@@ -146,8 +189,8 @@ function EmployeeDetails() {
                     type="email"
                     name="email"
                     value={employee.email}
-                    onChange={handleChange}
                     size="sm"
+                    disabled // Email cannot be changed
                   />
                 </Form.Group>
               </Col>
@@ -157,74 +200,110 @@ function EmployeeDetails() {
                   <Form.Control
                     type="text"
                     name="phone"
-                    value={employee.phone}
+                    value={employee.phone || ""}
                     onChange={handleChange}
                     size="sm"
                   />
                 </Form.Group>
               </Col>
             </Row>
-            <Row className="mb-2">
-              <Col xs={12} md={6}>
-                <Form.Group controlId="position">
-                  <Form.Label className="small-text">Position</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="position"
-                    value={employee.position}
-                    onChange={handleChange}
-                    size="sm"
-                  />
-                </Form.Group>
-              </Col>
-              <Col xs={12} md={6}>
-                <Form.Group controlId="paychex_id">
-                  <Form.Label className="small-text">Paychex ID</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="paychex_id"
-                    value={employee.paychex_id || ''}
-                    onChange={handleChange}
-                    size="sm"
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="mb-2">
-              <Col xs={12} md={6}>
-                <Form.Group controlId="is_admin">
-                  <Form.Label className="small-text">Admin Privileges</Form.Label>
-                  <Form.Select
-                    name="is_admin"
-                    value={employee.is_admin ? 'true' : 'false'}
-                    onChange={handleChange}
-                    size="sm"
-                  >
-                    <option value="false">No</option>
-                    <option value="true">Yes</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-            </Row>
+            {isAdmin && (
+              <>
+                <Row className="mb-2">
+                  <Col xs={12} md={6}>
+                    <Form.Group controlId="firebase_uid">
+                      <Form.Label className="small-text">
+                        Firebase UID
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="firebase_uid"
+                        value={employee.firebase_uid}
+                        size="sm"
+                        disabled // Firebase UID should not be editable
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <Form.Group controlId="paychex_id">
+                      <Form.Label className="small-text">Paychex ID</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="paychex_id"
+                        value={employee.paychex_id || ""}
+                        onChange={handleChange}
+                        size="sm"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row className="mb-2">
+                  <Col xs={12} md={6}>
+                    <Form.Group controlId="position">
+                      <Form.Label className="small-text">Position</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="position"
+                        value={employee.position || ""}
+                        onChange={handleChange}
+                        size="sm"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <Form.Group controlId="is_admin">
+                      <Form.Label className="small-text">
+                        Admin Privileges
+                      </Form.Label>
+                      <Form.Select
+                        name="is_admin"
+                        value={employee.is_admin ? "true" : "false"}
+                        onChange={handleChange}
+                        size="sm"
+                      >
+                        <option value="false">No</option>
+                        <option value="true">Yes</option>
+                      </Form.Select>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </>
+            )}
           </Form>
           <div className="text-center mt-3">
-            <Button variant="primary" size="sm" className="mx-1" onClick={handleSave}>Save Changes</Button>
             <Button
-  variant="danger"
-  size="sm"
-  className="mx-1"
-  onClick={handleDelete}
-  disabled={employee.firebase_uid === '7Yyz3S2X2iU5drTdZE65r8bxoCB2'} // Disable button for your account
->
-  Delete Employee
-</Button>
-
-            <Button variant="dark" size="sm" className="mx-1" onClick={() => navigate(-1)}>Back</Button>
+              variant="primary"
+              size="sm"
+              className="mx-1"
+              onClick={handleSave}
+            >
+              Save Changes
+            </Button>
+            {isAdmin && (
+              <Button
+                variant="danger"
+                size="sm"
+                className="mx-1"
+                onClick={handleDelete}
+              >
+                Delete Employee
+              </Button>
+            )}
+            <Button
+              variant="dark"
+              size="sm"
+              className="mx-1"
+              onClick={() => navigate(-1)}
+            >
+              Back
+            </Button>
           </div>
         </Card.Body>
       </Card>
     </Container>
   );
+  
+
 }
 
 export default EmployeeDetails;
