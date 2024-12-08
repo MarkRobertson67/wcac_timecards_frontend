ca4d2a6d57dd3bfbdc15abd186bdc4b45ae01757
Mark Robertson <markrobertson67@gmail.com>
2024-12-07 23:20:55 -0500

    Added delete buttons to each time input

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 // Copyright (c) 2024 Mark Robertson
 // See LICENSE.txt file for details.
 
+## Version .5.0 beta (2024-04-01)
+- Fixing bugs
 
-## Version 1.1 (2024-12-01)
+## Version 1.1 (2024-12-**)
 - Completed
--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -48,9 +48,9 @@ function FooterComponent() {
           </div>
         </div>
       </div>
-      <div className="text-center py-2 bg-secondary">
-        <span>© 2024 We Care Adult Care Timecards App™<br></br> by Mark Robertson. All Rights Reserved.</span>
-        <p>Version: {packageJson.version}</p> {/* Display version from package.json */}
+      <div className="text-center py-1 bg-secondary">
+        <span>© 2024 We Care Adult Care Timecards App™ by Mark Robertson.</span>
+        <p>All Rights Reserved. Version: {packageJson.version}</p> {/* Display version from package.json */}
 
       </div>
     </footer>
