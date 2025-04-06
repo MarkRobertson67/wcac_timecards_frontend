22b0dd816266f387a062b90c58500aef77a2a18c
Mark Robertson <markrobertson67@gmail.com>
2025-04-06 14:43:57 -0400

    Correcting spacing on Create New Time Card

--- src/components/pages/CreateNewTimecard/CreateNewTimecard.js
+++ src/components/pages/CreateNewTimecard/CreateNewTimecard.js
@@ -2,11 +2,11 @@
 // Copyright (c) 2024 Mark Robertson
 // See LICENSE.txt file for details.
 
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import styles from './CreateNewTimeCard.module.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "./CreateNewTimeCard.module.css";
 
 function CreateNewTimeCard({ setIsNewTimeCardCreated }) {
   const [startDate, setStartDate] = useState(new Date());
@@ -16,17 +16,17 @@ function CreateNewTimeCard({ setIsNewTimeCardCreated }) {
     setStartDate(date);
   };
 
-   // Call this method on logout or when you need to ensure fresh data is loaded
+  // Call this method on logout or when you need to ensure fresh data is loaded
   const clearLocalStorage = () => {
-    localStorage.removeItem('activeTimeCard');
-    localStorage.removeItem('startDate');
+    localStorage.removeItem("activeTimeCard");
+    localStorage.removeItem("startDate");
   };
 
   const formatDate = (date) => {
     // Format the date as YYYY-MM-DD
     const year = date.getFullYear();
-    const month = (`0${date.getMonth() + 1}`).slice(-2); // Adding 1 since getMonth() returns 0-11
-    const day = (`0${date.getDate()}`).slice(-2);
+    const month = `0${date.getMonth() + 1}`.slice(-2); // Adding 1 since getMonth() returns 0-11
+    const day = `0${date.getDate()}`.slice(-2);
     return `${year}-${month}-${day}`;
   };
 
@@ -34,28 +34,35 @@ function CreateNewTimeCard({ setIsNewTimeCardCreated }) {
     clearLocalStorage();
     // Format the date and store it in localStorage
     const formattedDate = formatDate(startDate);
-    localStorage.setItem('startDate', formattedDate);
+    localStorage.setItem("startDate", formattedDate);
 
     setIsNewTimeCardCreated(true);
-    navigate('/activeTimeCard', { state: { startDate: formattedDate } });
+    navigate("/activeTimeCard", { state: { startDate: formattedDate } });
   };
 
   return (
     <div className={styles.container}>
-      <div className="flex-container">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className={styles.card}>
-            <div className={styles.cardBody}>
-              <h2 className={styles.cardTitle}>Create New Time Card</h2>
-              <Calendar 
-                onChange={handleStartDateChange} 
-                value={startDate} 
-                locale="en-US" 
-              />
-              <button 
-                onClick={handleStartDateSelection} 
+            {/* text-center to center all elements inside */}
+            <div className={`${styles.cardBody} text-center`}>
+              <h2 className={styles.cardTitle} style={{ marginBottom: "40px" }}>
+                Create New Time Card
+              </h2>
+
+              <div className="d-flex justify-content-center">
+                <Calendar
+                  onChange={handleStartDateChange}
+                  value={startDate}
+                  locale="en-US"
+                />
+              </div>
+
+              <button
+                onClick={handleStartDateSelection}
                 className={`btn btn-primary ${styles.button}`}
+                style={{ marginTop: '40px' }}
               >
                 View / Create Time Card
               </button>
@@ -63,11 +70,8 @@ function CreateNewTimeCard({ setIsNewTimeCardCreated }) {
           </div>
         </div>
       </div>
-      </div>
     </div>
   );
 }
 
 export default CreateNewTimeCard;
-
-
